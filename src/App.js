e7c39499dd8946f5a5930cd00915e4d8324f095d
CesarPetit21480 <cesarpetit@gmail.com>
2022-12-22 20:06:27 -0300

    Se agreggo toastify

--- src/App.js
+++ src/App.js
@@ -7,27 +7,34 @@ import Error404 from "./component/Error404/Error404";
 import ItemDetailContainer from "./component/ItemDetailContainter/ItemDetailContainer";
 import ItemListContainer from "./component/ItemListContainer/ItemListContainer";
 import NavBar from "./component/NavBar/NavBar";
+import {toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   const [show, setshow] = useState(false);
 
-
   const [carrito, setCarrito] = useState([]);
-  const [count, setcount] = useState(0)
+  const [count, setcount] = useState(0);
 
-const addCarrito = (item) => {  
-  setCarrito([...carrito, item]);
+  const addCarrito = (item) => {
+    setCarrito([...carrito, item]);
     setcount(count + 1);
+    showToastMessage();
+  
+  };
 
-    console.log("el Carrito", carrito)
-}
-
+  const showToastMessage = () => {
+    console.log('showToastMessage');
+    
+    toast.success('Producto Agregado al Carrito!!!', {     
+      position: toast.POSITION.BOTTOM_RIGHT,
+    });
+};
 
   return (
     <div className="App">
-   
       <Router>
-        <NavBar count = {count} />
+        <NavBar count={count} />
         <Routes>
           <Route
             path="/"
@@ -43,17 +50,24 @@ const addCarrito = (item) => {
             path="/item/:cat"
             element={
               <ItemListContainer
-              bienvenida="BIENVENIDO AL PROYECTO"
-              nombre="CESAR PETIT"              
-              />              
+                bienvenida="BIENVENIDO AL PROYECTO"
+                nombre="CESAR PETIT"
+              />
             }
           />
           <Route path="/counter" element={<Counter />} />
-          <Route path="/item/detail/:id" element={<ItemDetailContainer addCarrito = {addCarrito} />} />
-          <Route path="/carrito/detail" element={<CarritoDetail carrito= {carrito} />} />          
+          <Route
+            path="/item/detail/:id"
+            element={<ItemDetailContainer addCarrito={addCarrito} />}
+          />
+          <Route
+            path="/carrito/detail"
+            element={<CarritoDetail carrito={carrito} />}
+          />
           <Route path="*" element={<Error404 />} />
         </Routes>
-      </Router>    
+      </Router>
+      <ToastContainer />
     </div>
   );
 }
